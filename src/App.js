01d2badbf5eff3ace487fbedda78007ae2b56ec1
Barbaros <67686040+Barbaros-Dev@users.noreply.github.com>
2021-10-03 01:28:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import AnaSayfa from "./components/AnaSayfa";
-import Panel from "./components/Panel";
-import Giriş from "./components/Giriş";
-import Profil from "./components/Profil";
 import Error from "./components/404";
-import Düzenle from "./components/Profil-Düzenle";
 
 function App() {
   return (
