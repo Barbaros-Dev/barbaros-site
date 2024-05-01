7e10c7aa4a5ecfa986a06b645dde0a353bd8fae4
Barbaros <aesadkocamis@gmail.com>
2024-05-01 18:21:27 +0300

    add portfolio

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ function App() {
   const [screenWidth, setScreenWidth] = useState()
   const [description] = useState([
     "Ben developer'ım",
-    "Ben front-end geliştiriciyim",
+    "Ben full-stack geliştiriciyim",
     "Ben back-end severim"
   ])
-
-  const projects = [
+  const [skills] = useState([
+    { name: "HTML", color: "#e14e1d", percentage: 85 },
+    { name: "CSS", color: "#0277bd", percentage: 85 },
+    { name: "JavaScript", color: "#f0db4f", percentage: 80 },
+    { name: "Python", color: "#306998", percentage: 15 }
+  ])
+  const [projects] = useState([
     {
       name: "ÖvGeç",
       image: "https://cdn.glitch.me/57fac641-81be-4f1a-b8ab-d3f8edb1751e%2F%C3%B6vge%C3%A7-ikon.png",
@@ -27,17 +32,39 @@ function App() {
       image: "https://play-lh.googleusercontent.com/LVjfFZSbghKize8WKIzOvvg408MY5koDdE6BFxR-yfnmzdQpNl9Kz1i2mUL8uBigFguw=w240-h480",
       link: "https://play.google.com/store/apps/details?id=com.mootie.sevgisayaci"
     },
-    {
-      name: "Dersteyim",
-      image: "https://dersteyim-yeni.vercel.app/logo.png",
-      link: "https://dersteyim-yeni.vercel.app"
-    },
+    //{
+    //  name: "Dersteyim",
+    //  image: "https://dersteyim-yeni.vercel.app/logo.png",
+    //  link: "https://dersteyim-yeni.vercel.app"
+    //},
     {
       name: "Geribildirim App",
       image: "https://geribildirim.vercel.app/logo.png",
       link: "https://geribildirim.vercel.app"
     }
-  ]
+  ])
+
+  const [works] = useState([
+    {
+      name: "VIPMUN'23",
+      image: "/assets/vipmun.png",
+      link: "https://vipmun.com",
+      description: "MUN websitesi",
+      technologies: [
+        "Next.js"
+      ]
+    },
+    {
+      name: "ALMUN'24",
+      image: "/assets/almun.png",
+      link: "https://almun24.com",
+      description: "MUN websitesi",
+      technologies: [
+        "Next.js",
+        "Tailwind CSS"
+      ]
+    }
+  ])
 
   useEffect(() => {
     setScreenWidth(window.screen.width)
@@ -75,14 +102,40 @@ function App() {
             <FaInstagram className='icon' size={25}></FaInstagram>
           </a>
         </div>
-        <a href='#projects' className='scroll'>
-          Projeler
+        <a href='#portfolio' className='scroll'>
+          Portfolyo
           <FaCaretDown size={25}></FaCaretDown>
         </a>
       </div>
-      <div className='seperator'></div>
-      <div id='projects' className='projects'>
-        <h1 className='projectsHeader'>Projeler</h1>
+      <div className='separator'></div>
+      <div id='portfolio' className='section projects'>
+        <h1 className='sectionHeader'>Portfolyo</h1>
+        <div className='worksContainer'>
+          {works.map((work, i) => (
+            <div className='work' key={i}>
+              <label className='workName'>{work.name}</label>
+              <div style={{ position: "relative", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <img draggable="false" alt={work.name} width={480} height={270} className='workImage' src={work.image}></img>
+                <div className='workContent'>
+                  {/*<label className='workDescription'>{work.description}</label>*/}
+                  <div className='technologiesContainer'>
+                    {/*<label>Kullanılan teknolojiler:</label>*/}
+                    <div className='technologies'>
+                      {work.technologies.map((tech, i) => (
+                        <label key={i} className='technology'>{tech}</label>
+                      ))}
+                    </div>
+                  </div>
+                  <a href={work.link} target='blank'><button className='workButton'>Websitesine git</button></a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='separator'></div>
+      <div id='projects' className='section projects'>
+        <h1 className='sectionHeader'>Projeler</h1>
         <div className='projectsContainer'>
           {projects.map((project, i) => (
             <a target='blank' href={screenWidth < 700 && project.mobile ? project.mobile : project.link} className='project' key={i}>
@@ -92,7 +145,7 @@ function App() {
           ))}
         </div>
       </div>
-      <div className='seperator'></div>
+      <div className='separator'></div>
       <div className='footer'>
         <div className='footerText'><a target='blank' href='https://discord.com/users/708579930979565588' className='footerName'>Ahmet Esad Kocamış</a> tarafından 💖 ile geliştirildi.</div>
       </div>
